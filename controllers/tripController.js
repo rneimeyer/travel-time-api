1564f755104a90ca0202f3e352768d392c15d91b
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-25 16:14:58 -0700

    created trip controller

---
+++ controllers/tripController.js
@@ -0,0 +1,60 @@
+//trip controller
+const express = require('express');
+const router = express.Router();
+
+//import model
+const Trip = require("./../models/Trip")
+
+//returns all trips
+router.get("/", (req, res) => {
+    Trip.find({}).then((trip) => {
+        res.json({
+            status: 200,
+            trip: travelltriper,
+        });
+    });
+});
+
+//returns a single trip and its data
+router.get("/:id", (req, res) => {
+    Trip.findById(req.params.id).then((trip) => {
+        res.json({
+            status: 200,
+            trip: trip,
+        })
+    })
+})
+
+//create new trip
+router.post("/", (req, res) => {
+    const tripData = req.body;
+    Trip.create(tripData).then((trip) => {
+        res.json({
+            status: 200,
+            trip: trip,
+        })
+    })
+})
+
+//update trip
+router.put("/:id", (req, res) => {
+    Trip.findByIdAndUpdate(req.params.id, req.body, { new: true }).then((trip) => {
+        res.json({
+            status: 200,
+            trip: trip,
+        })
+    })
+})
+
+//delete trip
+router.delete("/:id", (req, res) => {
+    Trip.findByIdAndDelete(req.params.id).then((trip) => {
+        res.json({
+            status: 200,
+            trip: trip,
+        })
+    })
+})
+
+
+module.exports = router;
