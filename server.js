44f1d572d99eaba9651dd422edb21db2dc780e4d
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-25 16:26:33 -0700

    updated server.js with routes

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const PORT = process.env.PORT
-
+const travellerController = require('./controllers/travellerController')
+const tripController = require('./controllers/tripController')
+const hotelController = require('./controllers/hotelController')
+const flightController = require('./controllers/flightController')
 
 const app = express()
 app.use(express.urlencoded({extended:false}))
@@ -13,8 +16,13 @@ app.use(logger('dev'))
 app.use(cors())
 
 app.get('/', function(req, res) {
-    res.send('Welcome')
+    res.send('Welcome to the Travel Time API. Please try /traveller, /trip, /hotel, or /flight to see more')
 })
+
+app.use('/traveller', travellerController)
+app.use('/trip', tripController)
+app.use('/hotel', hotelController)
+app.use('/flight', flightController)
 
 
 app.listen(PORT, () => {console.log(`listening on port ${PORT}`)})
