4531138d308f90039bbd5145453b05194ec05945
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-25 16:01:17 -0700

    updated connection file

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1/vinyl-api")
-// mongoose.connect("mongodb://localhost/vinyl-api")
+mongoose.connect("mongodb://127.0.0.1/travel-time-api")
+// mongoose.connect("mongodb://localhost/travel-time-api")
 
 module.exports = mongoose
