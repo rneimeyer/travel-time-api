fa987fb2cf515dbc3e5ad4a24ec4047a794e433d
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-31 12:11:02 -0700

    updated flight model to contain updated details

--- models/Flight.js
+++ models/Flight.js
@@ -3,8 +3,14 @@ const mongoose = require("./../db/connection");
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-  name: String,
-  link: String,
+    flight_ype: String,
+  airline: String,
+  flight_number: String,
+  date: Date,
+  starting_airport: String,
+  ending_airport: String,
+  departure_time: String,
+  arrival_time: String,
   price: Number
 });
 
