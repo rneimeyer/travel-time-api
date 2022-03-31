4e02c1857d5e04b66f8cc4559f6c08e1a8b511ba
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-31 13:00:36 -0700

    updated hotel model with new details

--- models/Hotel.js
+++ models/Hotel.js
@@ -3,9 +3,12 @@ const mongoose = require("./../db/connection");
 const Schema = mongoose.Schema;
 
 const hotelSchema = new Schema({
-  name: String,
-  link: String,
-  price: Number
+  hotel: String,
+  room_type: String,
+  check_in: Date,
+  check_out: Date,
+  price: Number,
+  ammenities: String
 });
 
 module.exports = mongoose.model("Hotel", hotelSchema);
