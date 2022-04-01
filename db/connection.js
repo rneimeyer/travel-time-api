d772471ed2e94bb8cf782d8277e2a65a9cdfd377
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-04-01 15:35:35 -0700

    setting up connection for deployment

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1/travel-time-api")
+let mongoURL = "";
+
+if (process.env.NODE_ENV === "production") {
+  mongoURL = process.env.DB_URL;
+} else {
+  mongoURL = "mongodb://127.0.0.1/travel-time-api";
+}
+
+mongoose.connect(mongoURL);
+
+// mongoose.connect("mongodb://127.0.0.1/travel-time-api")
 // mongoose.connect("mongodb://localhost/travel-time-api")
 
-module.exports = mongoose
+module.exports = mongoose;
