4727aaccf4decf05f0bc1a2cb47b165cc8f0d47b
Rebecca Neimeyer <rebeccaneimeyer@RebeccasMacBook.hsd1.ca.comcast.net>
2022-03-31 20:44:50 -0700

    added datea nd notes to schema

--- models/Activity.js
+++ models/Activity.js
@@ -4,8 +4,10 @@ const Schema = mongoose.Schema;
 
 const activitySchema = new Schema({
   activity: String,
+  date: Date,
   time: String,
   price: Number,
+  notes: String,
 });
 
 module.exports = mongoose.model("Activity", activitySchema);
